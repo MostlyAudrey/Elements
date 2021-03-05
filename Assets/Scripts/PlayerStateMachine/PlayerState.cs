4b4ad67f01026dc919227a48a8afa1ff228c421d
MultiDream <luka.derado@gmail.com>
2021-03-04 22:46:58 -0500

    Added 'Player State Script'
    This script sets up a player State that tracks health / invlunerability. It's not hooked up yet, but that comes next.

---
+++ Assets/Scripts/PlayerStateMachine/PlayerState.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerState: MonoBehaviour
+{
+    private Animator anim;
+    public int Health = 10;
+    public bool Hurt = false;
+
+    public void TakeDamage(int amount)
+    {
+        AnimatorStateInfo astate = anim.GetCurrentAnimatorStateInfo(0);
+        if (! astate.IsName("hit")){
+            Health = Health -= amount;
+            if (Health < 0)
+            {
+                onDeath();
+                return;
+            } else
+            {
+                Health--;
+                Timer timer = new Timer();
+                timer.setTimer(90f,notHurtAnymore);
+                timer.active = true;
+            }
+        }
+    }
+
+    public void onDeath()
+    {
+        // tell the animator you are dead.
+        anim.SetBool("Dead", true);
+    }
+
+    public void notHurtAnymore()
+    {
+        Hurt = true;
+    }
+
+}
