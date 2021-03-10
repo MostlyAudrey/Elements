5096b47d8ef00fa9b5cf37bade1e37e6eb5701f7
MultiDream <luka.derado@gmail.com>
2021-03-09 20:17:13 -0500

    PlayerState script allows player to get hurt.
    PlayerState script allows player to get hurt.

--- Assets/Scripts/PlayerStateMachine/PlayerState.cs
+++ Assets/Scripts/PlayerStateMachine/PlayerState.cs
@@ -11,23 +11,27 @@ public class PlayerState: MonoBehaviour
 {
     private Animator anim;
     public int Health = 10;
-    public bool Hurt = false;
+
+    // This actually might be handles by the animator already,
+    // based on the length of the Exit time of the animation state 'Hit'
+
+    public bool Hurt = false; // Hurt handles whether the character is invincible. (invincibility frames)
+    public float invincibleTime = 1.5f;
 
     public void TakeDamage(int amount)
     {
         AnimatorStateInfo astate = anim.GetCurrentAnimatorStateInfo(0);
-        if (! astate.IsName("hit")){
+        if (!astate.IsName("hit") && !astate.IsName("Death")){
             Health = Health -= amount;
             if (Health < 0)
             {
                 onDeath();
                 return;
-            } else
+            } 
+            else 
             {
-                Health--;
                 Timer timer = new Timer();
-                timer.setTimer(90f,notHurtAnymore);
-                timer.active = true;
+                timer.setTimer(invincibleTime, notHurtAnymore);
             }
         }
     }
@@ -36,11 +40,16 @@ public class PlayerState: MonoBehaviour
     {
         // tell the animator you are dead.
         anim.SetBool("Dead", true);
+
+        /* And here's where I'd put my respawn logic!*
+         * 
+         * ... IF I HAD ANY!
+         * */
     }
 
     public void notHurtAnymore()
     {
-        Hurt = true;
+        Hurt = false;
     }
 
 }
