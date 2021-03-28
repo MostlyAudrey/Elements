7c057bae7c48b3a1160e26dead0c9f11654bca54
Audrey <audreyyarmo@gmail.com>
2021-03-28 15:24:16 -0400

    Merged the bank updates into Game world

--- Assets/Scripts/Enviroment/MovableItem.cs
+++ Assets/Scripts/Enviroment/MovableItem.cs
@@ -15,40 +15,65 @@ class MovableItem : MonoBehaviour
     public float moveSpeed = 2;
     private int isSupposedToMove = 0; // -1 back, 1 forward, 0 neutral
 
+    private  GameObject target;
+    public GameObject buttonHint;       // A corresponding game object that 'holds a hint' related to the completion of the quest.
+    public bool showButtonHint    = true;         // Will the corresponding hint be shown?
+    public float buttonHintRadius = 5.0f;
+    protected bool showingHint;   
+
     public void pushButton()
     {
-      if (moveDistance <= positionOffset)
-      {
-        isSupposedToMove = -1;
-      } else {
-        isSupposedToMove = 1;
-      }
+        if ( isSupposedToMove == 0 )
+            isSupposedToMove = moveDistance <= positionOffset ? -1 : 1;
     }
 
     void Update()
     {
-      if (isSupposedToMove == 0)
-      {
-        gameObject.tag = "Button";
-        return;
-      } else if (isSupposedToMove == 1) {
-          gameObject.tag = "Untagged";
-          if (positionOffset < moveDistance)
-          {
-            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
-            positionOffset += 1 * moveSpeed * Time.deltaTime;
-          } else {
-            isSupposedToMove = 0;
-          }
-      } else if (isSupposedToMove == -1) {
-          gameObject.tag = "Untagged";
-          if (positionOffset > 0)
-          {
-            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
-            positionOffset += -1 * moveSpeed * Time.deltaTime;
-          } else {
-              isSupposedToMove = 0;
-          }
+        if ( showButtonHint )
+        {
+            if ( !target ) 
+                target = GameObject.FindGameObjectWithTag("Player");
+            if ( !showingHint && Vector3.Distance(target.transform.position, transform.position) <= buttonHintRadius)
+              _displayButtonHint();
+            else if ( showingHint && Vector3.Distance(target.transform.position, transform.position) > buttonHintRadius )
+              _hideButtonHint();
         }
-      }
+
+
+        if (isSupposedToMove == 0)
+        {
+            gameObject.tag = "Button";
+            return;
+        } else if (isSupposedToMove == 1) 
+        {
+            gameObject.tag = "Untagged";
+            if (positionOffset < moveDistance)
+            {
+                transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+                positionOffset += 1 * moveSpeed * Time.deltaTime;
+            } else isSupposedToMove = 0;
+
+        } else if (isSupposedToMove == -1) {
+            gameObject.tag = "Untagged";
+            if (positionOffset > 0)
+            {
+                transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+                positionOffset += -1 * moveSpeed * Time.deltaTime;
+            } else {
+                isSupposedToMove = 0;
+            }
+        }
+    }
+
+    private void _displayButtonHint()
+    {
+        showingHint = true;
+        if (showButtonHint) buttonHint.SetActive(true);
+    }
+
+    private void _hideButtonHint()
+    {
+        showingHint = false;
+        if (showButtonHint) buttonHint.SetActive(false);
+    }
 }
