376e7991eb35b3fff6c6bdc24427dfd4fd6d59e0
necromancer512 <43522920+necromancer512@users.noreply.github.com>
2021-04-10 16:20:33 -0400

    Add asynchronous loading for scenes with loading screen

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/**
+ * Singleton class for handling the process of going from one scene
+ * to another. Uses a loading screen and asynchronous loading to disguise stuttering.
+ *
+ * By Aneet Nadella
+ */
+public class SceneLoader : MonoBehaviour
+{
+    // Make sure only one instance exists in scene
+    private static SceneLoader instance;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public static SceneLoader Get()
+    {
+        if (instance == null)
+        {
+            Debug.LogError("SceneLoader does not exist within scene");
+        }
+        return instance;
+    }
+
+    [Tooltip("Prefab of loading screen")]
+    public GameObject loadingScreenPrefab;
+    private LoadingScreen loadingScreen = null;
+
+    private World worldToLoad;
+
+    /**
+     * Call to unload current world and load desired world.
+     * @param world Desired world.
+     */
+    public void GoToWorld(World world)
+    {
+        // Check if already loading a world
+        if (loadingScreen != null)
+        {
+            return;
+        }
+
+        Debug.Log("Go to world: " + world);
+        // Create loading screen
+        loadingScreen = Instantiate(loadingScreenPrefab).GetComponent<LoadingScreen>();
+        loadingScreen.OnFadeIn += StartAsyncLoading;
+
+        worldToLoad = world;
+    }
+
+    /**
+     * Start async loading once loading screen fades in
+     * and initialize loading screen.
+     */
+    private void StartAsyncLoading()
+    {
+        AsyncOperation loadOperation = LoadingUtility.AsyncGoToWorld(worldToLoad);
+        loadingScreen.Init(loadOperation);
+    }
+
+    /**
+     * Call to load current world again asynchronously with loading screen.
+     */
+    public void ReloadCurrentWorld()
+    {
+        GoToWorld((World) SceneManager.GetActiveScene().buildIndex);
+        Debug.Log((World) SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Scripts/UI/LoadingScreen.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/**
+ * Persistent loading screen for asynchronous loading between scenes.
+ * Fades in on its own. Make sure to call Init function to start updating the
+ * loading progress bar and to enable the loading screen to fade out when ready.
+ * 
+ * By Aneet Nadella
+ */
+public class LoadingScreen : MonoBehaviour
+{
+    [SerializeField]
+    private ProgressBar loadingProgressBar;
+    [SerializeField]
+    private CanvasGroup fadeGroup;
+    public float fadeDuration = 1f;
+
+    public UnityAction OnFadeIn;
+    public UnityAction OnFadeOut;
+
+    private AsyncOperation loadOperation = null;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        OnFadeOut += DestroyLoadingScreen;
+    }
+
+    void Start()
+    {
+        loadingProgressBar.SetPercentFill(0f);
+        // Start fade in
+        fadeGroup.alpha = 0f;
+        fadeGroup.LeanAlpha(1f, fadeDuration)
+        .setIgnoreTimeScale(true)
+        .setOnComplete(CallOnFadeIn);
+    }
+
+    public void Init(AsyncOperation loadOperation)
+    {
+        this.loadOperation = loadOperation;
+    }
+
+    void Update()
+    {
+        // Check if Init has been called
+        if (loadOperation != null)
+        {
+            // Update loading progress bar
+            loadingProgressBar.SetPercentFill(loadOperation.progress);
+
+            // Start fade out if appropriate
+            if (loadOperation.isDone)
+            {
+                fadeGroup.LeanAlpha(0f, fadeDuration)
+                //.setIgnoreTimeScale(true)
+                .setOnComplete(CallOnFadeOut);
+            }
+        }
+    }
+
+    private void DestroyLoadingScreen()
+    {
+        Destroy(gameObject);
+    }
+
+    // Wrapper functions to call UnityAction delegates since
+    // they are not of System.Action type
+    private void CallOnFadeIn() { OnFadeIn(); }
+    private void CallOnFadeOut() { OnFadeOut(); }
+}
