bb5711affd6167de885ecd9f8026e45a685cb79a
necromancer512 <43522920+necromancer512@users.noreply.github.com>
2021-04-13 09:37:07 -0400

    Make save system able to save multiple files

--- Assets/Scripts/Utility/SaveUtility.cs
+++ Assets/Scripts/Utility/SaveUtility.cs
@@ -6,23 +6,106 @@ using UnityEngine;
 
 public static class SaveUtility
 {
-    private static string saveFilePath = "/player_data.elements";
+    public static string saveFilePrefix = "/player_data";
+    public static string saveFileSuffix = ".elements";
     
     public static void SavePlayerData()
     {
         BinaryFormatter formatter = new BinaryFormatter();
-        string absolutePath = Application.persistentDataPath + saveFilePath;
-        FileStream stream = new FileStream(absolutePath, FileMode.Create);
+        string absolutePath = Application.persistentDataPath + GetSaveFilePath(System.DateTime.Now);
 
-        PlayerData data = new PlayerData();
+        FileStream stream = null;
+        try
+        {
+            stream = new FileStream(absolutePath, FileMode.Create);
+            PlayerData data = new PlayerData();
 
-        formatter.Serialize(stream, data);
-        stream.Close();
+            formatter.Serialize(stream, data);
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log("PlayerData: " + e.Message);
+        }
+        finally
+        {
+            if (stream != null)
+            {
+                stream.Close();
+            }
+        }
     }
 
+    /**
+     * Load the most recent save file.
+     */
     public static void LoadPlayerData()
     {
-        string absolutePath = Application.persistentDataPath + saveFilePath;
+        string saveDirPath = Application.persistentDataPath;
+        string[] saveFiles = null;
+        try 
+        {
+            saveFiles = Directory.GetFiles(saveDirPath);
+        }
+        catch (DirectoryNotFoundException e)
+        {
+            Debug.Log("PlayerData: " + e.Message);
+        }
+
+        if (saveFiles == null)
+        {
+            Debug.LogError("PlayerData could not find save files in save directory!");
+            return;
+        }
+
+        string lastSaveFile = null;
+        int i = 0;
+        while (i < saveFiles.Length)
+        {
+            // Make sure file is a save file by checking the suffix
+            if (saveFiles[i].Contains(saveFileSuffix))
+            {
+                if (lastSaveFile == null)
+                {
+                    lastSaveFile = saveFiles[i];
+                }
+                else
+                {
+                    // Check which file was created last
+                    try
+                    {
+                        if (File.GetCreationTime(lastSaveFile).CompareTo(File.GetCreationTime(saveFiles[i])) < 0)
+                        {
+                            lastSaveFile = saveFiles[i];
+                        }
+                    }
+                    catch (FileNotFoundException e) 
+                    {
+                        Debug.Log("PlayerData: " + e.Message);                        
+                    }
+                }
+            }
+
+            ++i;
+        }
+
+        if (lastSaveFile != null)
+        {
+            LoadPlayerData(lastSaveFile, true);
+        }
+        else
+        {
+            Debug.LogError("PlayerData could not find save file in " + saveDirPath);
+        }
+    }
+
+    /**
+     * Load a specific save file.
+     * @param saveFilePath Include the appropriate prefix, a timestamp, and a suffix.
+     * @param absolute Whether the save file path is absolute or not.
+     */
+    public static void LoadPlayerData(string saveFilePath, bool absolute)
+    {
+        string absolutePath = (absolute) ? saveFilePath : (Application.persistentDataPath + saveFilePath);
         if (File.Exists(absolutePath))
         {  
             BinaryFormatter formatter = new BinaryFormatter();
@@ -37,5 +120,14 @@ public static class SaveUtility
         {
             Debug.LogError("Save file not found at " + absolutePath);
         }
+    }
+
+    /**
+     * Helper method to get relative save file path.
+     * @param dt Date & time at which save file was created.
+     */
+    public static string GetSaveFilePath(System.DateTime dt)
+    {
+        return saveFilePrefix + dt.ToFileTime() + saveFileSuffix;
     }
 }
